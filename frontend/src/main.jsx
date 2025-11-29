dbf83380419f62c0ffc208f717cfc3198191ce56
russdoestech <138834284+russdoestech@users.noreply.github.com>
2025-11-29 23:17:40 +0000

    created a skeleton for vitejs, react, and react router, and started porting over the initial page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route} from 'react-router';
+import 'css/style.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.querySelector('main')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 )
