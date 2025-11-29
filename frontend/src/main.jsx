4f29b0b46d3434fe9b8c154c3631f8d1673c2591
russdoestech <138834284+russdoestech@users.noreply.github.com>
2025-11-29 21:02:40 +0000

    syncing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/stylesheets/style.css'
+import './index.css'
 import App from './App.jsx'
 
-createRoot(document.querySelector('main')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
